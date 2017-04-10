486e01bbd20fa29d3f719c77f9ac65a89ae25074
Jeffrey Leung <jleung513@gmail.com>
2017-04-09 23:24:50 -0700

    Change default screen to NFC scanning activity

--- application/app/src/main/java/com/lifeband/lifeband/activity/SplashActivity.java
+++ application/app/src/main/java/com/lifeband/lifeband/activity/SplashActivity.java
@@ -28,7 +28,7 @@ public class SplashActivity extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, NfcActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
