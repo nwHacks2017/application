1efc80ecf4c84f4f754ccac532679d3b827acd52
Jeffrey Leung <jleung513@gmail.com>
2018-01-08 11:59:31 -0800

    Combine polling for response with callback

--- application/app/src/main/java/com/lifeband/lifeband/activity/LoadingActivity.java
+++ application/app/src/main/java/com/lifeband/lifeband/activity/LoadingActivity.java
@@ -6,8 +6,6 @@ package com.lifeband.lifeband.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -28,14 +26,9 @@ import com.lifeband.lifeband.service.BackendClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.HttpURLConnection;
-import java.util.Optional;
-
 public class LoadingActivity extends AppCompatActivity {
 
     private static final String TAG = LoadingActivity.class.getSimpleName();
-
-    private Boolean successResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +40,7 @@ public class LoadingActivity extends AppCompatActivity {
         Log.d(TAG, "Receiving Extra tagData: " + tagData);
 
         getGlobalVars().setCurrentPatientData(null);
-        successResponse = false;
         requestPatientData(tagData);
-        waitForServerResponse();
     }
 
     private void requestPatientData(String tagData) {
@@ -71,75 +62,35 @@ public class LoadingActivity extends AppCompatActivity {
                         }
 
                         getGlobalVars().setCurrentPatientData(patientData);
+
+                        Intent newIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(newIntent);
                     }
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.i(TAG, error.toString());
                         error.printStackTrace();
+                        String errorMessage= "Empty error message";
 
                         if (error instanceof TimeoutError || error instanceof NoConnectionError) {
-                            Toast.makeText(
-                                    getApplication(),
-                                    "Could not connect to the server.",
-                                    Toast.LENGTH_LONG
-                            ).show();
+                            errorMessage = "Could not connect to the server.";
                         } else if (error instanceof AuthFailureError) {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "Failed to authenticate with the server.",
-                                    Toast.LENGTH_LONG
-                            ).show();
+                            errorMessage = "Failed to authenticate with the server.";
                         } else if (error instanceof ServerError) {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "The server returned an internal error.",
-                                    Toast.LENGTH_LONG
-                            ).show();
+                            errorMessage = "The server returned an internal error.";
                         } else if (error instanceof NetworkError) {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "Unknown etwork error.",
-                                    Toast.LENGTH_LONG
-                            ).show();
+                            errorMessage = "Unknown network error.";
                         } else if (error instanceof ParseError) {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "The server response could not be understood.",
-                                    Toast.LENGTH_LONG
-                            ).show();
+                            errorMessage = "The server response could not be understood.";
                         }
+
+                        Toast.makeText(getApplication(), errorMessage, Toast.LENGTH_LONG).show();
+                        finish();
                     }
                 });
 
         // Data is returned asynchronously
-    }
-
-    private void waitForServerResponse() {
-
-        successResponse = false;
-
-        new CountDownTimer(6000, 200) {
-
-            public void onTick(long millisUntilFinished) {
-                Log.d(TAG, "Checking if patient data has been received from the server.");
-                if (getGlobalVars().getCurrentPatientData() != null) {
-                    successResponse = true;
-                    Intent newIntent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(newIntent);
-                }
-            }
-
-            public void onFinish() {
-                if (!successResponse) {
-                    Log.i(TAG, "Timed out while retrieving patient data from server.");
-                    // Notification to user is handled by callback response
-                    finish();
-                }
-            }
-
-        }.start();
-
     }
 
     private GlobalVars getGlobalVars() {
